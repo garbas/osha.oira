dbb0353129472289ef2a3a87582d7eba66f453a3
Wolfgang Thomas <thomas@syslab.com>
2014-04-27 19:45:12 +0200

    add 2 more questions to the company survey (WIP, the data is not saved yet, since we need to extend the postgres schema). Refs #9281

--- src/osha/oira/client/company.py
+++ src/osha/oira/client/company.py
@@ -1,9 +1,43 @@
 from five import grok
+from zope import schema
+from zope.interface import directlyProvides
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm
 from z3c.form import button
+from euphorie.client import model
 from euphorie.client.company import Company as GenericCompany
+from euphorie.client.company import CompanySchema as GenericCompanySchema
+from euphorie.client.session import SessionManager
+# from plone.directives import form
 from .interfaces import IOSHAReportPhaseSkinLayer
+from .. import _
 
 grok.templatedir("templates")
+
+
+class CompanySchema(GenericCompanySchema):
+
+    needs_met = schema.Choice(
+        title=_(
+            "label_needs_met",
+            default=u"Did this OiRA tool meet to your needs?"),
+        vocabulary=SimpleVocabulary([
+            SimpleTerm(True, title=_("label_yes", default=u"Yes")),
+            SimpleTerm(False, title=_("label_no", default=u"No")),
+        ]),
+        required=False
+    )
+    recommend_tool = schema.Choice(
+        title=_(
+            "label_recommend_tool",
+            default=u"Would you recommend this OiRA tool to an enterprise "
+            u"similar to yours ?"),
+        vocabulary=SimpleVocabulary([
+            SimpleTerm(True, title=_("label_yes", default=u"Yes")),
+            SimpleTerm(False, title=_("label_no", default=u"No")),
+        ]),
+        required=False
+    )
 
 
 class Company(GenericCompany):
@@ -11,6 +45,17 @@ class Company(GenericCompany):
     """
     grok.layer(IOSHAReportPhaseSkinLayer)
     grok.template("report_company")
+
+    schema = CompanySchema
+
+    def _assertCompany(self):
+        if self.company is not None:
+            return
+        session = SessionManager.session
+        if session.company is None:
+            session.company = model.Company()
+        directlyProvides(session.company, CompanySchema)
+        self.company = session.company
 
     @button.buttonAndHandler(u"Previous")
     def handlePrevious(self, action):
@@ -37,7 +82,10 @@ class Company(GenericCompany):
             'country': u'xx',
             'employees': None,
             'referer': None,
-            'workers_participated': None}
+            'workers_participated': None,
+            'needs_met': None,
+            'recommend_tool': None,
+        }
         self.applyChanges(data)
         url = "%s/report/view" % self.request.survey.absolute_url()
         self.request.response.redirect(url)
